57d4897b8ce8c5f03c639c0a9a7c7cf17a79d597
plyn85 <plyn99@gmail.com>
2020-05-04 15:51:25 +0100

    updated static root

--- django_project/settings.py
+++ django_project/settings.py
@@ -125,7 +125,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 # STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'))
 # Media root is a path to a directory where we like django to store uploaded files
